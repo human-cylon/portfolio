4375e2d0f47cd98826f2266535e1743399b8a2e6
Alina Soboleva <alina.soboleva.spb@gmail.com>
2021-05-31 14:38:50 +0300

    feat: close button for hamburger menu

--- Shelter/src/index.js
+++ Shelter/src/index.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".header__menu")
+const menu = document.querySelector(".header__menu");
 
 menu.onmouseenter = function() {
   document.querySelector(".header__menu-item_active").classList.remove("header__menu-item_active");
@@ -6,4 +6,10 @@ menu.onmouseenter = function() {
 
 menu.onmouseleave = function() {
   document.querySelector(".header__menu-item").classList.add("header__menu-item_active");
+}
+
+const btnHamburger = document.querySelector(".hamburger-menu");
+
+btnHamburger.onclick = function() {
+  this.classList.toggle("open");
 }
